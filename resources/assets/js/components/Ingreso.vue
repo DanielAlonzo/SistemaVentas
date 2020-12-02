578a05a243e11241b45a0e887b91fcf78146d96b
Daniel <dani3plea@gmail.com>
2020-12-02 02:34:06 -0600

    VisualizarIngresos

--- resources/assets/js/components/Ingreso.vue
+++ resources/assets/js/components/Ingreso.vue
@@ -14,7 +14,7 @@
                         </button>
                     </div>
                     <!-- Listado-->
-                    <template v-if="listado">
+                    <template v-if="listado==1">
                     <div class="card-body">
                         <div class="form-group row">
                             <div class="col-md-6">
@@ -59,7 +59,7 @@
                                         <td v-text="ingreso.impuesto"></td>
                                         <td v-text="ingreso.estado"></td>
                                         <td>
-                                            <button type="button" @click="abrirModal('ingreso','actualizar',ingreso)" class="btn btn-success btn-sm">
+                                            <button type="button" @click="verIngreso(ingreso.id)" class="btn btn-success btn-sm">
                                             <i class="icon-eye"></i>
                                             </button> &nbsp;
                                             <template v-if="ingreso.estado=='Registrado'">
@@ -89,7 +89,7 @@
                     </template>
                     <!--Fin Listado-->
                     <!-- Detalle-->
-                    <template v-else>
+                    <template v-else-if="listado==0">
                     <div class="card-body">
                         <div class="form-group row border">
                             <div class="col-md-9">
@@ -238,6 +238,107 @@
                     </div>
                     </template>
                     <!-- Fin Detalle-->
+
+                    
+                     <!-- Ver Ingreso-->
+                        <template v-else-if="listado==2">
+                    <div class="card-body">
+                        <div class="form-group row border">
+                            <div class="col-md-9">
+                                <div class="form-group">
+                                    <label for="">Proveedor</label>
+                                    <label style="color:red">*</label>
+                                   <p v-text="proveedor"></p>
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <label for="">Impuesto</label>
+                                <label style="color:red">*</label>
+                                <p v-text="impuesto"></p>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>Tipo Comprobante</label>
+                                    <label style="color:red">*</label>
+                                    <p v-text="tipo_comprobante"></p>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>Serie Comprobante</label>
+                                    <p v-text="serie_comprobante"></p>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>Número Comprobante</label>
+                                    <label style="color:red">*</label>
+                                    <p v-text="num_comprobante"></p>
+                                </div>
+                            </div>
+
+                           
+                        </div>
+                       
+                        <div class="form-group row border">
+                            <div class="table-responsive col-md-12">
+                                <table class="table table-bordered table-striped table-sm">
+                                    <thead>
+                                        <tr>
+                                           
+                                            <th>Artículo</th>
+                                            <th>Precio</th>
+                                            <th>Cantidad</th>
+                                            <th>Subtotal</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody v-if="arrayDetalle.length">
+                                        <tr v-for="detalle in arrayDetalle" :key="detalle.id">
+                                           
+                                            <td v-text="detalle.articulo">
+                                             
+                                            </td>
+                                            <td v-text="detalle.precio">
+                                                
+                                            </td>
+                                            <td v-text="detalle.cantidad">
+                                               
+                                            </td>
+                                            <td>
+                                                {{detalle.precio*detalle.cantidad}}
+                                            </td>
+                                        </tr>
+                                       
+                                        <tr style="background-color: #CEECF5;">
+                                            <td colspan="4" align="right"><strong>Total Parcial:</strong></td>
+                                            <td>L. {{totalParcial=(total-totalImpuesto).toFixed(2)}}</td>
+                                        </tr>
+                                        <tr style="background-color: #CEECF5;">
+                                            <td colspan="4" align="right"><strong>Total Impuesto:</strong></td>
+                                            <td>L. {{totalImpuesto=((total*impuesto)/(1+impuesto)).toFixed(2)}}</td>
+                                        </tr>
+                                        <tr style="background-color: #CEECF5;">
+                                            <td colspan="4" align="right"><strong>Total Neto:</strong></td>
+                                            <td>L. {{total=calcularTotal}}</td>
+                                        </tr>
+                                    </tbody>  
+                                    <tbody v-else>
+                                        <tr>
+                                            <td  colspan="5">No hay articulos agregados</td>
+                                        </tr>
+                                    </tbody>                                  
+                                </table>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-md-12">
+                                <button type="button" @click="ocultarDetalle()" class="btn btn-danger">Cerrar</button>
+                               
+                            </div>
+                        </div>
+                    </div>
+                    </template>
+                    <!-- Final Ingreso-->
                 </div>
                 <!-- Fin ejemplo de tabla Listado -->
             </div>
@@ -342,6 +443,7 @@
             return {
                 ingreso_id: 0,
                 idproveedor:0,
+                proveedor:'',
                 nombre : '',
                 tipo_comprobante : 'Ticket',
                 serie_comprobante : '',
@@ -672,6 +774,9 @@
             },
             ocultarDetalle(){
                 this.listado=1;
+            },
+            verIngreso(id){
+                this.listado=2;
             },
             cerrarModal(){
                 this.modal=0;
